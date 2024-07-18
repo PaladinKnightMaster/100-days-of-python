6172e5fdc046eadca16f8da475adaaf594a84f13
CodeKnight <116281672+PaladinKnightMaster@users.noreply.github.com>
2024-07-17 21:22:57 -0700

    add day3

---
+++ Day3/main.py
@@ -0,0 +1,37 @@
+myName = input("What's your name? ")
+myLunch = input("What are you having for lunch? ")
+print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+
+
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName, "thinks it is", whatYear)
+
+
+
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called", person)
+print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
+
+
+
+foodType = input("Name a food: ")
+plantType = input("Name a plant: ")
+cookingMethod = input("Name a cooking method: ")
+burnedFood = input("What word describes burned food?: ")
+householdItem = input("Name a DYI item: ")
+
+print()
+
+print("MENU")
+print(cookingMethod, foodType, "with", burnedFood, plantType, "on a bed of", householdItem)
